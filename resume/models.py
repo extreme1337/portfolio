f160f605e0bb324afe5151092094146b1012091e
Marko Miseljic <marko.miseljic.14@gmail.com>
2021-01-27 13:56:07 +0100

    Added blog page

--- resume/models.py
+++ resume/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Resume(models.Model):
@@ -15,4 +16,16 @@ class Resume(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
     
--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Resume
+from .models import Resume, Post
+from django.views.generic import ListView
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,13 @@ def home(request):
 def about_page(request):
     resume = Resume.objects.get(pk=1)
     return render(request, 'resume/about.html', {'resume': resume})
+
+def blog(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request,'resume/blog.html', context)
+
+
+class PostListView(ListView):
+    model = Post
